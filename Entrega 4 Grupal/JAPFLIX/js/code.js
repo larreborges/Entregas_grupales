25f637d8665cdc75ae06f431aedc7bf3074cb737
larreborges <andresotero2@gmail.com>
2022-09-18 17:59:04 -0300

    Entrega 4

---
+++ Entrega 4 Grupal/JAPFLIX/js/code.js
@@ -0,0 +1,7 @@
+const getData = fetch('https://japceibal.github.io/japflix_api/movies-data.json')
+.then(response => response.json())
+.then(response => (response))
+
+document.getElementById("btnBuscar").addEventListener("click", function(){
+    
+})

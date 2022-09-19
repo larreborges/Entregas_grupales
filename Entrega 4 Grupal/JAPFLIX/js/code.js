a0c8b666b964869b9f5da33a0ee112ef89ecfaaf
larreborges <andresotero2@gmail.com>
2022-09-19 07:40:16 -0300

    Grupal

--- Entrega 4 Grupal/JAPFLIX/js/code.js
+++ Entrega 4 Grupal/JAPFLIX/js/code.js
@@ -2,50 +2,50 @@ const getData = fetch('https://japceibal.github.io/japflix_api/movies-data.json'
 .then(response => response.json())
 .then(response => (response))
 
-document.getElementById("btnBuscar").addEventListener("click", function(){
-
-    let informacion = document.getElementById("inputBuscar").value
-
-    if (movieList.title.includes(informacion) || movieList.genres.includes(informacion) || 
-    movieList.tagline.includes(informacion) || movieList.overview.includes(informacion)) {
-        
-
-
-    }
-
-})
-
-
-
 const showCategoriesList = async () => {
     const movieData = await getData;
-        let movieList = movieData
-        let htmlContentToAppend = ""; 
+    let movieList = movieData
 
-        for(let i=0; i < movieList.length; i++)   {         
-            let movieList = movieData[i]
-
-            htmlContentToAppend += `
-                <li><b>${movieList.title}</b><li>
-                <li><small> ${movieList.tagline}</small></li>
-                <li id = "valorar"></li>
-                `
-
-                for (let index = 0; index < movieList.vote_average; index++) {
-                    htmlContentToAppend += `
-                    <span class="fa fa-star checked"></span>
-                    `
-                }
-
-                for (let index = movieList.vote_average; index < 10; index++) {
-                    htmlContentToAppend += `
-                    <span class="fa fa-star"></span>
-                    `
-                }
-            }
-
-            document.getElementById("lista").innerHTML = htmlContentToAppend;
-            
-    }
+document.getElementById("btnBuscar").addEventListener("click", function(){
         
+                for(let i=0; i < movieList.length; i++)   {
+                    let htmlContentToAppend = ""; 
+                    let informacion = document.getElementById("inputBuscar").value
+                    let movieList = movieData[i]
+                    let movieGenres = movieList.genres[0].name.toLowerCase()
+                    let titulo = movieList.title.toLowerCase()
+                    let identificador = movieList.tagline.toLowerCase()
+                    console.log(informacion)
+                    console.log(titulo)
+        
+                    if (titulo.includes(informacion) || movieGenres.includes(informacion) || 
+                    identificador.includes(informacion) || movieList.overview.includes(informacion)) {
+
+                    htmlContentToAppend += `
+                        <li><b>${movieList.title}</b><li>
+                        <li><small> ${movieList.tagline}</small></li>
+                        <li id = "valorar"></li>
+                        `
+        
+                        for (let index = 0; index < movieList.vote_average; index++) {
+                            htmlContentToAppend += `
+                            <span class="fa fa-star checked"></span>
+                            `
+                        }
+        
+                        for (let index = movieList.vote_average; index < 10; index++) {
+                            htmlContentToAppend += `
+                            <span class="fa fa-star"></span>
+                            `
+                        }
+                    }
+        
+                    document.getElementById("lista").innerHTML = htmlContentToAppend;
+                    
+            }
+                
+    }
+
+)}
+
 showCategoriesList();

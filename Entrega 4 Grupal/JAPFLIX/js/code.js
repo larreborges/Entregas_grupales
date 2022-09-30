733534219612514b0b5958d02afa81a5093ec768
larreborges <andresotero2@gmail.com>
2022-09-29 22:43:02 -0300

    Grupal

--- Entrega 4 Grupal/JAPFLIX/js/code.js
+++ Entrega 4 Grupal/JAPFLIX/js/code.js
@@ -7,24 +7,34 @@ const showCategoriesList = async () => {
     let movieList = movieData
 
 document.getElementById("btnBuscar").addEventListener("click", function(){
-        
+    let htmlContentToAppend = "";
                 for(let i=0; i < movieList.length; i++)   {
-                    let htmlContentToAppend = ""; 
+                     
                     let informacion = document.getElementById("inputBuscar").value
                     let movieList = movieData[i]
                     let movieGenres = movieList.genres[0].name.toLowerCase()
                     let titulo = movieList.title.toLowerCase()
                     let identificador = movieList.tagline.toLowerCase()
-                    console.log(informacion)
-                    console.log(titulo)
         
                     if (titulo.includes(informacion) || movieGenres.includes(informacion) || 
                     identificador.includes(informacion) || movieList.overview.includes(informacion)) {
 
                     htmlContentToAppend += `
                         <li><b>${movieList.title}</b><li>
-                        <li><small> ${movieList.tagline}</small></li>
+                        <li><small>${movieList.tagline}</small></li>
                         <li id = "valorar"></li>
+                        <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasTop" aria-controls="offcanvasTop">Toggle top offcanvas</button>
+
+                        <div class="offcanvas offcanvas-top" tabindex="-1" id="offcanvasTop" aria-labelledby="offcanvasTopLabel">
+                            <div class="offcanvas-header">
+                                <h5 class="offcanvas-title" id="offcanvasTopLabel" style="color:black;" >${movieList.title}</h5>
+                                <p style="color:black;">${movieList.overview}</p>
+                                <p> </p>
+                                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                                </div>
+                                <div class="offcanvas-body">
+                                    </div>
+                                </div>
                         `
         
                         for (let index = 0; index < movieList.vote_average; index++) {
@@ -39,7 +49,7 @@ document.getElementById("btnBuscar").addEventListener("click", function(){
                             `
                         }
                     }
-        
+                    
                     document.getElementById("lista").innerHTML = htmlContentToAppend;
                     
             }

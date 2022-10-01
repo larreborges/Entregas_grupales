3c41ea131e010d7ad5158044cb6d590b0344e9dd
larreborges <andresotero2@gmail.com>
2022-10-01 18:23:39 -0300

    Se

--- Entrega 4 Grupal/JAPFLIX/js/code.js
+++ Entrega 4 Grupal/JAPFLIX/js/code.js
@@ -1,61 +1,68 @@
-const getData = fetch('https://japceibal.github.io/japflix_api/movies-data.json')
-.then(response => response.json())
-.then(response => (response))
+const getData = fetch("https://japceibal.github.io/japflix_api/movies-data.json")
+  .then((response) => response.json())
+  .then((response) => response);
 
 const showCategoriesList = async () => {
-    const movieData = await getData;
-    let movieList = movieData
+  const movieData = await getData;
+  let movieList = movieData;
 
-document.getElementById("btnBuscar").addEventListener("click", function(){
+  document.getElementById("btnBuscar").addEventListener("click", function () {
     let htmlContentToAppend = "";
-                for(let i=0; i < movieList.length; i++)   {
-                     
-                    let informacion = document.getElementById("inputBuscar").value
-                    let movieList = movieData[i]
-                    let movieGenres = movieList.genres[0].name.toLowerCase()
-                    let titulo = movieList.title.toLowerCase()
-                    let identificador = movieList.tagline.toLowerCase()
-        
-                    if (titulo.includes(informacion) || movieGenres.includes(informacion) || 
-                    identificador.includes(informacion) || movieList.overview.includes(informacion)) {
+    for (let i = 0; i < movieList.length; i++) {
+      let informacion = document.getElementById("inputBuscar").value;
+      let movieList = movieData[i];
+      let movieGenres = movieList.genres[0].name.toLowerCase();
+      let titulo = movieList.title.toLowerCase();
+      let identificador = movieList.tagline.toLowerCase();
 
-                    htmlContentToAppend += `
+      if (
+        titulo.includes(informacion) ||
+        movieGenres.includes(informacion) ||
+        identificador.includes(informacion) ||
+        movieList.overview.includes(informacion)
+      ) {
+        htmlContentToAppend += `
+        <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasTop" aria-controls="offcanvasTop">
                         <li><b>${movieList.title}</b><li>
                         <li><small>${movieList.tagline}</small></li>
-                        <li id = "valorar"></li>
-                        <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasTop" aria-controls="offcanvasTop">Toggle top offcanvas</button>
-
+                        </button>
                         <div class="offcanvas offcanvas-top" tabindex="-1" id="offcanvasTop" aria-labelledby="offcanvasTopLabel">
                             <div class="offcanvas-header">
                                 <h5 class="offcanvas-title" id="offcanvasTopLabel" style="color:black;" >${movieList.title}</h5>
                                 <p style="color:black;">${movieList.overview}</p>
-                                <p> </p>
+                                <p>${movieList.genres} </p>
+                                <div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false"> More </button>
+  <ul class="dropdown-menu">
+    <li><a class="dropdown-item" href="#">Year: ${movieList.release_date} </a></li>
+    <li><a class="dropdown-item" href="#">Runtime: ${movieList.runtime} mins </a></li>
+    <li><a class="dropdown-item" href="#">Budget: $${movieList.budget} </a></li>
+    <li><a class="dropdown-item" href="#">Revenue: $${movieList.revenue} </a></li>
+  </ul>
+</div>
                                 <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                                 </div>
                                 <div class="offcanvas-body">
                                     </div>
                                 </div>
-                        `
-        
-                        for (let index = 0; index < movieList.vote_average; index++) {
-                            htmlContentToAppend += `
-                            <span class="fa fa-star checked"></span>
-                            `
-                        }
-        
-                        for (let index = movieList.vote_average; index < 10; index++) {
-                            htmlContentToAppend += `
-                            <span class="fa fa-star"></span>
-                            `
-                        }
-                    }
-                    
-                    document.getElementById("lista").innerHTML = htmlContentToAppend;
-                    
-            }
-                
-    }
+                        `;
 
-)}
+        for (let index = 0; index < movieList.vote_average; index++) {
+          htmlContentToAppend += `
+                            <span class="fa fa-star checked"></span>
+                            `;
+        }
+
+        for (let index = movieList.vote_average; index < 10; index++) {
+          htmlContentToAppend += `
+                            <span class="fa fa-star"></span>
+                            `;
+        }
+      }
+
+      document.getElementById("lista").innerHTML = htmlContentToAppend;
+    }
+  });
+};
 
 showCategoriesList();

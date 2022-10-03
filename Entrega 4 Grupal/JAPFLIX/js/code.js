767647f8ff44f2a9ede82afef51aae0c38576041
larreborges <andresotero2@gmail.com>
2022-10-02 21:30:04 -0300

    Final

--- Entrega 4 Grupal/JAPFLIX/js/code.js
+++ Entrega 4 Grupal/JAPFLIX/js/code.js
@@ -13,31 +13,38 @@ const showCategoriesList = async () => {
     let informacion = document.getElementById("inputBuscar").value;
 
     for (let i = 0; i < movieList.length; i++) {
-      let movieList = movieData[i];
-      let movieGenres = movieList.genres[0].name.toLowerCase();
+      let movieList = movieData[i]; 
+      let movieGenres =  movieList.genres;
       let titulo = movieList.title.toLowerCase();
       let identificador = movieList.tagline.toLowerCase();
-      
+      let anio = movieList.release_date.substr(0,4);
+      let peliGeneros = [];
+
+      for (let index = 0; index < movieGenres.length; index++) {
+        peliGeneros +=  movieGenres[index].name + " "
+      }
+
       if (
         titulo.includes(informacion) ||
-        movieGenres.includes(informacion) ||
+        peliGeneros.includes(informacion) ||
         identificador.includes(informacion) ||
         movieList.overview.includes(informacion)
       ) {
+
         htmlContentToAppend += `
-          <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasTop" aria-controls="offcanvasTop">
-            <li><b>${movieList.title}</b><li>
-            <li><small>${movieList.tagline}</small></li>
+          <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasTop${i}" aria-controls="offcanvasTop">
+            <p><b>${movieList.title}</b><p>
+            <p><small>${movieList.tagline}</small></p>
           </button>
-            <div class="offcanvas offcanvas-top" tabindex="-1" id="offcanvasTop" aria-labelledby="offcanvasTopLabel">
+            <div class="offcanvas offcanvas-top" tabindex="-1" id="offcanvasTop${i}" aria-labelledby="offcanvasTopLabel">
               <div class="offcanvas-header">
                 <h5 class="offcanvas-title" id="offcanvasTopLabel" style="color:black;">${movieList.title}</h5>
                   <p style="color:black;">${movieList.overview}</p>
-                  <p style="color:black;">${movieList.genres.name}</p>
+                  <p style="color:black;">${peliGeneros}</p>
                     <div class="dropdown">
                       <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false"> More </button>
                         <ul class="dropdown-menu">
-                          <li><a class="dropdown-item" href="#">Year: ${movieList.release_date} </a></li>
+                          <li><a class="dropdown-item" href="#">Year: ${anio} </a></li>
                           <li><a class="dropdown-item" href="#">Runtime: ${movieList.runtime} mins </a></li>
                           <li><a class="dropdown-item" href="#">Budget: $${movieList.budget} </a></li>
                           <li><a class="dropdown-item" href="#">Revenue: $${movieList.revenue} </a></li>
